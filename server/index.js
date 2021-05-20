448ff97c209723e4f352890158247f41928085c0
samius-s <serg.mypost@gmail.com>
2021-05-20 07:13:27 +0300

    create database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('start?')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+
+const startApp = async () => {
+    try {
+        await mongoose.connect(config.get('dbUrl'), { useUnifiedTopology: true, useNewUrlParser: true })
+
+        app.listen(PORT, () => {
+            console.log('Server started on port', PORT)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
