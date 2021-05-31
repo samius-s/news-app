967256a55349fe8ff0dc0e1843e2409953bb9d91
samius-s <serg.mypost@gmail.com>
2021-05-31 20:17:57 +0300

    corret fullNewsItem

--- client/src/components/full-news-item/full-news-item.jsx
+++ client/src/components/full-news-item/full-news-item.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './full-news-item.css'
 
-const FullNewsItem = ({ id, newsItem }) => {
+const FullNewsItem = ({ newsItem }) => {
 
     const { fullDescription, image, title, shortDescription } = newsItem
+
+    const fullDescriptionEdit = fullDescription.split('\\n').map((text) => {
+        return (
+            <p>{text}</p>
+        )
+    })
 
     const imgUrl = `http://localhost:5000/newsImage-${image}.jpg`
 
@@ -11,8 +17,12 @@ const FullNewsItem = ({ id, newsItem }) => {
         <article className='full-news-list'>
             <h2>{title}</h2>
             <p>{shortDescription}</p>
-            <img src={imgUrl} alt='cover' />
-            <p>{fullDescription}</p>
+            <div className='image-container'>
+                <img src={imgUrl} alt='cover' />
+            </div>
+
+            {fullDescriptionEdit}
+            {/* <p>{fullDescription}</p> */}
 
         </article>
     )
